72c498affc4ed1142f5085c7a505c352d19ecdc9
Giacomo Zanotti <giacomozanotti@cuebiq.com>
2022-12-18 16:22:07 +0100

    remove unsued css, fastened animations

--- assets/js/animate-box.js
+++ assets/js/animate-box.js
@@ -1,20 +1,12 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const box = document.getElementsByTagName("box")[0]
-    const wrapper = document.getElementsByClassName("laptop-wrapper")[0]
     
     setTimeout(() => {
-        box.style.animation = 'start ease-in-out 1.5s 1'
+        box.style.animation = 'start ease-in-out 1s 1'
         box.style.animationFillMode = 'forwards'
     },
     //text animation ends after 5s 
-    5200);
-
-    setTimeout(() => {
-        wrapper.style.animation = 'visibilityOk ease-in-out 1.5s 1'
-        wrapper.style.animationFillMode = 'forwards'
-    },
-    //text animation ends after 5s 
-    5400);
+    2600);
 
 });
 
