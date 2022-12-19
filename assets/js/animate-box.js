a0482c791f7fab12d0933a9fc0ed17279e84b472
Giacomo Zanotti <giacomozanotti@cuebiq.com>
2022-12-19 22:04:09 +0100

    added cool gif

--- assets/js/animate-box.js
+++ assets/js/animate-box.js
@@ -1,12 +1,17 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const box = document.getElementsByTagName("box")[0]
-    
+    const imageHolder = document.getElementsByClassName("responsive_image")[0]
     setTimeout(() => {
         box.style.animation = 'start ease-in-out 1s 1'
         box.style.animationFillMode = 'forwards'
     },
-    //text animation ends after 5s 
-    2600);
+        //text animation ends after 5s 
+        2600);
+
+    setTimeout(() => {
+        imageHolder.style.animation = 'center ease-in-out 1s 1'
+        imageHolder.style.animationFillMode = 'forwards'
+    }, 2750);
 
 });
 
