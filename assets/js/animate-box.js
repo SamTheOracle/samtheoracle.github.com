441b43347190de9b9d8fe13f96e7f11aef780733
Giacomo Zanotti <giacomozanotti@cuebiq.com>
2022-12-20 22:00:31 +0100

    finish landing page

--- assets/js/animate-box.js
+++ assets/js/animate-box.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     const box = document.getElementsByTagName("box")[0]
     const imageHolder = document.getElementsByClassName("responsive_image")[0]
+    const iconHolder = document.getElementsByClassName("icon_holder")[0]
     setTimeout(() => {
         box.style.animation = 'start ease-in-out 1s 1'
         box.style.animationFillMode = 'forwards'
@@ -12,6 +13,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
         imageHolder.style.animation = 'center ease-in-out 1s 1'
         imageHolder.style.animationFillMode = 'forwards'
     }, 2750);
+
+    setTimeout(() => {
+        iconHolder.style.animation = 'end ease-in-out 1s 1'
+        iconHolder.style.animationFillMode = 'forwards'
+    }, 2950);
+
 
 });
 
