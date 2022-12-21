c64e1f640f9f3d306ddc5df17e8ab88ae8bd5ac4
Giacomo Zanotti <giacomozanotti@cuebiq.com>
2022-12-21 14:56:01 +0100

    better download link

--- assets/js/landingpage.js
+++ assets/js/landingpage.js
@@ -2,12 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     const box = document.getElementsByTagName("box")[0]
     const imageHolder = document.getElementsByClassName("responsive_image")[0]
     const iconHolder = document.getElementsByClassName("icon_holder")[0]
-    const downloadIcon = document.getElementById("downloadIcon")
-
-    downloadIcon.addEventListener("click", (event) => {
-        event.preventDefault()
-        window.open("https://drive.google.com/file/d/1gkiZrfVcdBDII5WCqDs4duDqIFv878Il/view?usp=share_link", "_blank")
-    })
+    
     setTimeout(() => {
         box.style.animation = 'start ease-in-out 1s 1'
         box.style.animationFillMode = 'forwards'
